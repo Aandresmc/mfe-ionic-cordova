85dbe6f18708345f432dc4cbad5eb26df3b4780f
Andres Mora Castro <amorac@intergrupo.com>
2022-05-04 23:20:24 -0500

    setup config

---
+++ projects/myApp/webpack.config.js
@@ -0,0 +1,72 @@
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const mf = require("@angular-architects/module-federation/webpack");
+const path = require("path");
+const share = mf.share;
+
+const sharedMappings = new mf.SharedMappings();
+sharedMappings.register(path.join(__dirname, "tsconfig.json"), [
+  /* mapped paths to share */
+]);
+
+module.exports = {
+  output: {
+    uniqueName: "myApp",
+    publicPath: "auto",
+  },
+  optimization: {
+    runtimeChunk: false,
+  },
+  resolve: {
+    alias: {
+      ...sharedMappings.getAliases(),
+    },
+  },
+  experiments: {
+    outputModule: true,
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      library: { type: "module" },
+
+      // For remotes (please adjust)
+      // name: "myApp",
+      // filename: "remoteEntry.js",
+      // exposes: {
+      //     './Component': './/src/app/app.component.ts',
+      // },
+
+      // For hosts (please adjust)
+      remotes: {
+        // mf: "http://localhost:4242/remoteEntry.js",
+        mf: "https://mfe-ionic.web.app/remoteEntry.js",
+        // mf: "https://cors-anywhere.herokuapp.com/https://mfe-ionic.web.app/remoteEntry.js",
+      },
+      shared: share({
+        "@angular/core": {
+          singleton: true,
+          strictVersion: true,
+          requiredVersion: "auto",
+          // requiredVersion: ">=1.1.0 <3.0.0"
+        },
+        "@angular/common": {
+          singleton: true,
+          strictVersion: true,
+          requiredVersion: "auto",
+        },
+        "@angular/common/http": {
+          singleton: true,
+          strictVersion: true,
+          requiredVersion: "auto",
+        },
+        "@angular/router": {
+          singleton: true,
+          strictVersion: true,
+          requiredVersion: "auto",
+        },
+
+        ...sharedMappings.getDescriptors(),
+      }),
+    }),
+    sharedMappings.getPlugin(),
+  ],
+};
